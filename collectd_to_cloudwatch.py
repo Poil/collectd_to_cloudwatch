146b3fc7a1d06650f8365692d95f0892dcb93f34
Benjamin Dupuis <benjamin.dupuis@fr.clara.net>
2015-11-23 11:11:31 +0100

    Add instance-id and better configuration system

--- collectd_to_cloudwatch.py
+++ collectd_to_cloudwatch.py
@@ -1,5 +1,6 @@
 import collectd
 import boto.ec2.cloudwatch
+import boto.utils
 from yaml import load as yload
 
 REGION = False
@@ -8,11 +9,12 @@ AWS_SECRET_ACCESS_KEY = False
 NAMESPACE = "COLLECTD"
 METRICS = {}
 cw_ec2 = False
+INSTANCE_ID = False
 
 
 def config(conf):
     collectd.debug('Configuring Stuff')
-    global REGION, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, NAMESPACE, METRICS
+    global REGION, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, NAMESPACE, METRICS, INSTANCE_ID
 
     for node in conf.children:
         if node.key == 'region':
@@ -26,12 +28,18 @@ def config(conf):
         if node.key == 'metrics_config':
             metrics_config = node.values[0]
 
-        collectd.debug('Loading YAML plugins configuration')
+    if not metrics_config:
+        collectd.warning("Missing YAML plugins configuration please define metrics_config")
+    
+    collectd.debug('Loading YAML plugins configuration')
     try:
         stream = open(metrics_config)
         METRICS = yload(stream)
     except:
         collectd.warning(("Couldn't load YAML plugins configuration {0}").format(metrics_config))
+
+    # get instance ID
+    INSTANCE_ID = boto.utils.get_instance_metadata()['instance-id']
 
 
 def init():
@@ -54,28 +62,33 @@ def shutdown():
 
 
 def write(vl, datas=None):
-    global cw_ec2, NAMESPACE, METRICS
+    global cw_ec2, NAMESPACE, METRICS, INSTANCE_ID
 
-    # Get unit for current p/t/ti, if not exists, do nothing
+    # Get config for current p/t, if not exists, do nothing
     if METRICS.get(vl.plugin) and METRICS[vl.plugin].get(vl.type):
+        # Get default plugin unit
+        unit = METRICS[vl.plugin][vl.type].get('unit', 'None')
 
+        # Build Metric Name (like FS because I can't have a beautifull CamelCase with collectd's name)
         if vl.plugin_instance:
             metric_name = '{p}-{pi}/{t}'.format(p=vl.plugin, pi=vl.plugin_instance, t=vl.type)
         else:
             metric_name = '{p}/{t}'.format(p=vl.plugin, t=vl.type)
 
-        # Append type_instance to metric_name and get unit of the metric
+        # Append type_instance to metric_name and get the unit of the metric if exists
         if vl.type_instance:
             metric_name = '{m}-{ti}'.format(m=metric_name, ti=vl.type_instance)
-            unit = METRICS[vl.plugin][vl.type][vl.type_instance]
-        else:
-            unit = METRICS[vl.plugin][vl.type]
+            if METRICS[vl.plugin][vl.type].get('type_instance', False):
+                unit = METRICS[vl.plugin][vl.type]['type_instance'].get(vl.type_instance, unit)
 
-        dimensions = {'Source': 'collectd', 'InstanceName': vl.host}
+        dimensions = {'InstanceId': INSTANCE_ID, 'InstanceName': vl.host}
         # Needed ?
         for i in vl.values:
             collectd.debug(('Putting {metric}={value} {unit} to {namespace} {dimensions}').format(metric=metric_name, value=i, unit=unit, namespace=NAMESPACE, dimensions=dimensions))
-            cw_ec2.put_metric_data(namespace=NAMESPACE, name=metric_name, value=float(i), unit=unit, dimensions=dimensions)
+            try:
+                cw_ec2.put_metric_data(namespace=NAMESPACE, name=metric_name, value=float(i), unit=unit, dimensions=dimensions)
+            except:
+                collectd.debug(('Fail to put {metric}={value} {unit} to {namespace} {dimensions}').format(metric=metric_name, value=i, unit=unit, namespace=NAMESPACE, dimensions=dimensions))
 
 
 collectd.register_config(config)
